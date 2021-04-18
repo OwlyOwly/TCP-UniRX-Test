03862e82abbc92ca9839510ea6bb5293aa0e1f6e
Ruslan <sadykovlq@gmail.com>
2021-04-18 15:49:36 +0300

    Initial commit

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    [SerializeField] private GameObject explosionEffectGO;
+    [SerializeField] private GameObject lightGO;
+
+    public void ActivateExplosion()
+    {
+        explosionEffectGO.SetActive(true);
+    }
+
+    public void SwitchOffLight()
+    {
+        lightGO.SetActive(false);
+    }
+
+    public void SwitchOnLight()
+    {
+        lightGO.SetActive(true);
+    }
+}
